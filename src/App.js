0fa54be5f207b57fc00c6bafc4b9a55ffcfd85bd
unknown <rifatullah1418@gmail.com>
2021-09-24 23:33:41 +0600

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './componants/Header/Header';
+import Shop from './componants/Shop/Shop';
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Shop></Shop>
     </div>
   );
 }
